c12fa5da19260487c499618a0af68418413fd571
asdfg <3439838993@qq.com>
2025-10-03 14:53:17 +0800

    Program5 compare different threshold approaches on noisy low rank data

--- SVD_PROGRAMS.py
+++ SVD_PROGRAMS.py
@@ -35,7 +35,7 @@ xtilde=VT.T@np.linalg.inv(np.diag(S))@U.T@b          #least-square fit
 plt.plot(a,xtilde*a)
 plt.show()
 
-#Program4 pca on noisy cloud of data
+'''#Program4 pca on noisy cloud of data
 #generate noisy cloud of data
 xC=np.array([2,1])      #center of data
 sig=np.array([2,0.5])   #principal axes
@@ -47,4 +47,32 @@ Xavg=np.mean(X,axis=1)
 B=X-np.tile(Xavg,(nPoints,1)).T
 #SVD and PCA
 U,S,VT=np.linalg.svd(B/np.sqrt(nPoints),full_matrices=False)
-theta=2*np.pi*np.arange(0,1,0.01)
+theta=2*np.pi*np.arange(0,1,0.01)'''
+
+#Program5 compare different threshold approaches on noisy low rank data
+t=np.arange(-3,3,0.01)
+Utrue=np.array([np.cos(17*t)*np.exp(-t**2),np.sin(11*t)]).T
+Strue=np.array([[2,0],[0,0.5]])
+Vtrue=np.array([np.sin(5*t)*np.exp(-t**2),np.cos(13*t)]).T
+
+X=Utrue@Strue@Vtrue.T
+plt.imshow(X)
+plt.show()
+#contaminate signal with noise
+sigma=1
+Xnoisy=X+sigma*np.random.randn(*X.shape)
+plt.imshow(Xnoisy)
+plt.show()
+#truncate using optimal hard threshold
+U,S,VT=np.linalg.svd(X,full_matrices=False)
+N=Xnoisy.shape[0]
+cutoff=(4/np.sqrt(3)*np.sqrt(N)*sigma)#hard threshold
+r=np.max(np.where(S>cutoff))
+Xclean=U[:,:(r+1)]@np.diag(S[:r+1])@VT[:(r+1),:]
+plt.imshow(Xclean)
+plt.show()
+cds=np.cumsum(S)/np.sum(S)
+r90=np.min(np.where(cds>0.9))
+X90=U[:,:(r90+1)]@np.diag(S[:r90+1])@VT[:(r90+1),:]
+plt.imshow(X90)
+plt.show()
